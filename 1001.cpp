7bd94466e713f241aeb6a2d63d840dc14ed53a63
CuteMeta777 <84311539+CuteMeta777@users.noreply.github.com>
2026-03-15 22:08:01 +0900

    Create 1001.cpp

---
+++ 1001.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void solution() 
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a - b << endl;
+}
+
+int main() 
+{
+    solution();
+    
+    return 0;
+}
