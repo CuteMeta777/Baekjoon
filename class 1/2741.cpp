5ac0659062f2a9a120657e08f6b0314f11fa2370
CuteMeta777 <84311539+CuteMeta777@users.noreply.github.com>
2026-03-18 18:27:13 +0900

    2741

--- class 1/2741.cpp
+++ class 1/2741.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 void solution(int N)
 {
-    for (int i = 1; i <= 9; i++)
-        cout << N << " * " << i << " = " << N * i << endl;
+    for (int i = 1; i <= N; i++)
+        cout << i << "\n";
 }
 
 int main(void)
