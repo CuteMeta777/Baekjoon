bb499e6f3320fe5d6821954436132f54f3256335
CuteMeta777 <84311539+CuteMeta777@users.noreply.github.com>
2026-03-18 18:40:25 +0900

    10171

---
+++ class 1/10171.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void solution()
+{
+    printf("\\    /\\");
+    printf("\n");
+    printf(" )  ( ')");
+    printf("\n");
+    printf("(  /  )");
+    printf("\n");
+    printf(" \\(__)|");
+}
+
+int main(void)
+{
+    solution();
+
+    return 0;
+}
