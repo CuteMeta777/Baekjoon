e00773bfa860aa6297479eb0279fa0fdef33770d
CuteMeta777 <eigensoyul@gmail.com>
2026-03-20 14:07:32 +0900

    Update 10250.cpp

--- class 1/10250.cpp
+++ class 1/10250.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-void solution()
+void solution(const int& T)
 {
+    int H, W, N;
+    int floor, room_number;
+    for (int i = 0; i < T; i++)
+    {
+        cin >> H >> W >> N;
 
+        floor = N % H;
+        if (N % H == 0) floor = H;
+
+        room_number = (N / H) + 1;
+        if (N % H == 0) room_number--;
+
+        cout << floor;
+        if (room_number < 10)
+            cout << '0' << room_number;
+        else
+            cout << room_number;
+        cout << "\n";
+    }
 }
 
 int main(void)
 {
-    solution();
+    int T;
+    cin >> T;
+
+    solution(T);
 
     return 0;
 }
