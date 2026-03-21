275532e784dc85e611541d960619ee471fb160ce
CuteMeta777 <eigensoyul@gmail.com>
2026-03-21 21:48:42 +0900

    Update 30802.cpp

--- class 2/30802.cpp
+++ class 2/30802.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void solution()
+void solution(const int& N, const vector<int>& sizes, const int& T, const int& P)
 {
+    int shirt_bundle, pen_bundle, pen_left;
 
+    shirt_bundle = 0;
+    for (int i = 0; i < sizes.size(); i++)
+    {
+        shirt_bundle += (sizes[i] / T);
+        if (sizes[i] % T != 0)
+            shirt_bundle++;
+    }
+
+    pen_bundle = N / P;
+    pen_left = N % P;
+
+    cout << shirt_bundle << "\n";
+    cout << pen_bundle << " " << pen_left;
 }
 
 int main(void)
 {
-    solution();
+    int N;
+    vector<int> sizes(6);
+    int T, P;
+
+    cin >> N;
+    for (int& e : sizes)
+        cin >> e;
+    cin >> T >> P;
+
+    solution(N, sizes, T, P);
 
     return 0;
 }
