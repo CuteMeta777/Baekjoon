a91a3b0196f15c7f853c030a07424ebf28b0f59b
CuteMeta777 <eigensoyul@gmail.com>
2026-03-27 21:29:56 +0900

    Update 2805.cpp

--- class 3/2805.cpp
+++ class 3/2805.cpp
@@ -11,7 +11,39 @@ void fast_IO() // fold it at start
 
 void solution(const long long& N, const long long& M, const vector<long long>& heights)
 {
+    long long max_height = *max_element(heights.begin(), heights.end());
+
+    long long left = 0;
+    long long right = max_height;
+
+    long long mid;
+    long long total_wood;
+
+    long long answer;
     
+    while (left <= right)
+    {
+        total_wood = 0;
+        mid = (left + right) / 2;
+
+        for (int i = 0; i < N; i++)
+        {
+            if (heights[i] > mid)
+                total_wood += (heights[i] - mid);
+        }
+
+        if (total_wood >= M)
+        {
+            answer = mid;
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+
+    cout << answer;
 }
 
 int main(void)
@@ -22,6 +54,8 @@ int main(void)
     cin >> N >> M;
 
     vector<long long> heights(N);
+    for (long long& e : heights)
+        cin >> e;
 
     solution(N, M, heights);
 
