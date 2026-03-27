7cf9d86edfd3c2bb1e84ef892b5dc9e03e59be00
CuteMeta777 <eigensoyul@gmail.com>
2026-03-27 17:34:43 +0900

    block filling dp

---
+++ class 3/1012.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+void fast_IO() // fold it at start
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void solution(const int& T)
+{
+    for (int t = 0; t < T; t++)
+    {
+
+    }
+}
+
+int main(void)
+{
+    fast_IO();
+
+    int T;
+    cin >> T;
+
+    solution(T);
+
+    return 0;
+}
